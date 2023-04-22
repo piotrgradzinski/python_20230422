3c6d50c5581da1e34d49cf8ca81d2045dc06507d
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-22 13:58:14 +0200

    Ex 2.

--- basics_pgg/ex_2.py
+++ basics_pgg/ex_2.py
@@ -9,3 +9,21 @@ Weight: 2.5
 Total price: 25.0
 """
 
+price = 10.0
+weight = 2.5
+total_price = price * weight
+
+# 1 approach
+print('Price per kg:', price)
+print('Weight:', weight)
+print('Total price:', total_price)
+
+# 2 approach
+print('Price per kg: ' + str(price))
+print('Weight: ' + str(weight))
+print('Total price: ' + str(total_price))
+
+# 3 approach
+print(f'Price per kg: {price}')
+print(f'Weight : {weight}')
+print(f'Total price: {total_price}')
