3df21fdd9be08667f78506e28dea0298c2476427
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-22 12:28:59 +0200

    Ex 1.

--- basics_pgg/ex_1.py
+++ basics_pgg/ex_1.py
@@ -2,5 +2,14 @@
 """
 Using variable assignment, write a program to calculate
 the trapezoid field with base lengths 3 and 9 and height 6.5.
-Display the resulting area.
+Display the results.
+https://www.cuemath.com/measurement/area-of-trapezoid/
 """
+
+short_base = 3
+long_base = 9
+height = 6.5
+area = 1 / 2 * (short_base + long_base) * height
+
+print(area)
+
