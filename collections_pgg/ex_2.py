6920de5aadecfecd98bbb304f3c4990a2c57199e
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-23 15:47:28 +0200

    Ex 2.

---
+++ collections_pgg/ex_2.py
@@ -0,0 +1,6 @@
+"""
+Write a program that calculates the average of numbers provided by the user.
+To store the numbers use list. Allow up to 10 numbers to be entered.
+Use the `len()`, `sum()` built-in function.
+"""
+
