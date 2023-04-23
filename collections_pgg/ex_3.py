6c0e419a52d0f19f81d6a86173a0ef36b8e9500f
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-23 16:35:38 +0200

    Ex 3.

---
+++ collections_pgg/ex_3.py
@@ -0,0 +1,9 @@
+"""
+Write a program that counts occurrences of positive
+and negative numbers in a given list of integers.
+"""
+
+# 0 is neither positive nor negative
+
+numbers = [1, 2, 3, -100, -10, 0, 4]
+
