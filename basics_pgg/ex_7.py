37e4d7eb4f80405c50ceb4b21d30f4e008d65057
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-23 10:29:47 +0200

    Ex 7.

--- basics_pgg/ex_7.py
+++ basics_pgg/ex_7.py
@@ -15,3 +15,32 @@ The player is in the upper right corner.
 x = int(input('Enter player\'s X position: '))
 y = int(input('Enter player\'s Y position: '))
 
+if 0 <= x <= 10:
+    if 0 <= y <= 10:
+        print('left bottom corner')
+    elif 10 < y < 90:
+        print('left side')
+    elif 90 <= y <= 100:
+        print('left top corner')
+    else:
+        print('You are out of the board.')
+elif 10 < x < 90:
+    if 0 <= y <= 10:
+        print('bottom side')
+    elif 10 < y < 90:
+        print('center')
+    elif 90 <= y <= 100:
+        print('top side')
+    else:
+        print('You are out of the board.')
+elif 90 <= x <= 100:
+    if 0 <= y <= 10:
+        print('right bottom corner')
+    elif 10 < y < 90:
+        print('right side')
+    elif 90 <= y <= 100:
+        print('right top corner')
+    else:
+        print('You are out of the board.')
+else:
+    print('You are out of the board.')
