ecbfea2792717f1aa5909e7100cdc0d3bbee8884
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-23 16:46:55 +0200

    Ex 3.

--- collections_pgg/ex_3.py
+++ collections_pgg/ex_3.py
@@ -1,9 +1,20 @@
 """
 Write a program that counts occurrences of positive
 and negative numbers in a given list of integers.
-"""
 
-# 0 is neither positive nor negative
+https://en.wikipedia.org/wiki/0#Mathematics
+"0 is neither positive nor negative"
+"""
 
 numbers = [1, 2, 3, -100, -10, 0, 4]
 
+positive = 0
+negative = 0
+
+for number in numbers:
+    if number > 0:
+        positive += 1
+    elif number < 0:
+        negative += 1
+
+print(f'Positive: {positive}, negative: {negative}')
