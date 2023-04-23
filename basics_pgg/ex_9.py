8bac1e8bfaa9e8e69e5f8040e3431f707f0b2125
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-23 12:12:48 +0200

    Ex 9.

--- basics_pgg/ex_9.py
+++ basics_pgg/ex_9.py
@@ -8,3 +8,14 @@ for a given week based on temperatures entered by the user.
   the sum of temperatures by 7
 - display the results
 """
+
+day_number = 1
+temperature_sum = 0
+
+while day_number <= 7:
+    temperature_sum += float(input(f'Provide temperature for day {day_number}: '))
+    day_number += 1
+
+average_temperature = temperature_sum / 7
+
+print(f'Average temperature is {average_temperature:.2f}')
