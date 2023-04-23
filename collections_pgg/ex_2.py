28716665d3076e8b1b02113f13952ece6b7e1397
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-23 16:11:50 +0200

    Ex 2.

--- collections_pgg/ex_2.py
+++ collections_pgg/ex_2.py
@@ -4,3 +4,12 @@ To store the numbers use list. Allow up to 10 numbers to be entered.
 Use the `len()`, `sum()` built-in function.
 """
 
+numbers = []
+
+while len(numbers) < 10:
+    user_number = float(input('Provide a number: '))
+    numbers.append(user_number)
+
+average = sum(numbers) / len(numbers)
+
+print(f'The average of provided numbers is {average:.2f}')
