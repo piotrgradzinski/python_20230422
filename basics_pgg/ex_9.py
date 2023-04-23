9cb6f9cb688bbf155633e766c860fde83cd2b1ac
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-04-23 11:47:27 +0200

    Ex 9.

---
+++ basics_pgg/ex_9.py
@@ -0,0 +1,10 @@
+"""
+Write a program that calculates the average temperature
+for a given week based on temperatures entered by the user.
+
+- create temperature_sum variable = 0
+- using the while ask user 7 times to provide a temperature reading
+- after the while loop calculate the average by dividing
+  the sum of temperatures by 7
+- display the results
+"""
