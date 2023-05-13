2ebb5fd6f3204c3f5a8f4233e93759fb42c63079
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-13 15:02:48 +0200

    ex 9.

---
+++ collections_pgg/ex_9.py
@@ -0,0 +1,23 @@
+"""
+Write a program that counts the number of occurrences of each character in the string
+given by the user.
+
+1. We will create empty dictionary, where we will be counting letters.
+2. Get the string from the user.
+3. Iterate over the string and:
+    - character does not exist in dictionary (there is no key) - add it with value 0
+    - increment the value number by 1
+
+dictionary
+key  => value
+letter => number of occurrences (int)
+
+> To be or not to be
+t = 3
+o = 4
+  = 5
+b = 2
+e = 2
+r = 1
+n = 1
+"""
