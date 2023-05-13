6d562e1f5c45c618fb83040ff6a1c9d36447ad0a
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-13 09:55:57 +0200

    ex 4.

--- collections_pgg/ex_4.py
+++ collections_pgg/ex_4.py
@@ -12,4 +12,16 @@ Numbers divisible by 3 or 5:
 100
 
 In the range 0-100 there are 48 numbers divisible by 3 or 5
+
+Hint: how to check if number is divisible by 2?
+number % 2 == 0
 """
+
+how_many = 0
+for number in range(0, 101):
+    if number % 3 == 0 or number % 5 == 0:
+        print(number)
+        how_many += 1
+
+print()  # just for having an empty line
+print(f"In the range 0-100 there are {how_many} numbers divisible by 3 or 5")
