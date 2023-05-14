3d08a4a445fa146cfe45b788dca1f3f11e33048a
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-14 15:28:56 +0200

    ex 4.

---
+++ functions_pgg/ex_4.py
@@ -0,0 +1,18 @@
+"""
+Write a function that returns a set of all characters appearing
+more than a given number of times in a string.
+
+Example of use:
+more_than('mary had a little lamb', 3)
+{'a', ' '}
+
+1. Define function
+2. Count the number of characters in a string into a dictionary
+3. Go through this dictionary and if the number is greater than the number in second argument, add it to set.
+4. Return the set
+
+Test cases:
+empty string
+non-empty string
+a string with small/big letters
+"""
