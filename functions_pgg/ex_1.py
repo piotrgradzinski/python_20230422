65fa0335241de7a9615900f9b7ac7e4c6aa605b7
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-14 11:45:31 +0200

    ex 1.

--- functions_pgg/ex_1.py
+++ functions_pgg/ex_1.py
@@ -5,3 +5,35 @@ Create a simple, function based calculator
 3. Do the calculation and present results
 """
 
+
+def addition(number_1, number_2):
+    return number_1 + number_2
+
+def subtract(number_1, number_2):
+    return number_1 - number_2
+
+def multiply(number_1, number_2):
+    return number_1 * number_2
+
+def divide(number_1, number_2):
+    return number_1 / number_2
+
+users_number_1 = float(input('Provide number 1: '))
+users_number_2 = float(input('Provide number 2: '))
+operation = input('Provide operation (+, -, *, /): ')
+
+if operation == '+':
+    result = addition(users_number_1, users_number_2)
+elif operation == '-':
+    result = subtract(users_number_1, users_number_2)
+elif operation == '*':
+    result = multiply(users_number_1, users_number_2)
+elif operation == '/':
+    result = divide(users_number_1, users_number_2)
+else:
+    result = None
+
+if result is not None:
+    print(f'{users_number_1} {operation} {users_number_2} = {result}')
+else:
+    print('Unknown operation! Try again!')
