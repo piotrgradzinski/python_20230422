4680b75894cb02f9183d6b42c9e5d086188e68a0
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-14 14:38:10 +0200

    ex 3.

--- functions_pgg/ex_3.py
+++ functions_pgg/ex_3.py
@@ -43,3 +43,13 @@ def is_prime(number: int) -> bool:
             return False
 
     return True
+
+
+print(is_prime(-10))  # False
+print(is_prime(0))  # False
+print(is_prime(1))  # False
+print(is_prime(2))  # True
+print(is_prime(3))  # True
+print(is_prime(4))  # False
+print(is_prime(5))  # True
+print(is_prime(10))  # False
