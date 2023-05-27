eca88cc872b5317fee3852809f6df89043854f3a
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-27 10:09:34 +0200

    Functions, ex 6.

---
+++ functions_pgg/ex_6.py
@@ -0,0 +1,7 @@
+"""
+Create a function that will calculate an average value from its arguments.
+Leverage *args mechanism for that.
+Function invoked without arguments should return None
+
+ex. average(10, 20, 30) -> 20
+"""
--- functions_pgg/function_intro.py
+++ functions_pgg/function_intro.py
@@ -95,3 +95,119 @@ print(wrapper(text='To be or not to be', end='@@@'))
 # With this approach we have to provide positional arguments first and
 # then named arguments in any order we want
 print(wrapper('To be or not to be', end='$$$'))
+
+print('-' * 30)
+
+# One line if
+
+person = {
+    # 'id': 123,
+    'first_name': 'Piotr',
+    'last_name': 'GG'
+}
+
+# standard approach
+if 'id' in person:
+    has_id = True
+else:
+    has_id = False
+
+print(has_id)
+
+"""
+one-liner approach
+some restrictions:
+- we have to provide both values when condition evaluates to True and to False
+- you can return anything you want
+- for simple operations
+- when having more operations to be done within certain code block (if/else) use a standard approach
+"""
+has_id = True if 'id' in person else False
+print(has_id)
+
+has_id = 'Exists' if 'id' in person else 'Missing'
+print(has_id)
+
+print('-' * 30)
+
+"""
+Something to bool conversion. 
+We have quite a few functions that allow us to convert data types:
+- int(3.14) - will convert float to int
+- float() - to float
+- bool() - to boolean: True, False
+- str() - to string
+- list() - to list
+- etc.
+"""
+
+print(bool('Piotr'))  # str -> bool, True
+print(bool(' '))  # str -> bool, True
+print(bool(''))  # str -> bool, False
+# empty string -> False, non-empty string -> True
+
+print(bool(10))  # int -> bool, True
+print(bool(-5))  # int -> bool, True
+print(bool(0))  # int -> bool, False
+# 0 -> False, anything else -> True
+
+print(bool('0'))  # str -> bool, True
+
+print(bool([1, 2, 3]))  # list -> bool, True
+print(bool([]))  # list -> bool, False
+# this applies to other collections as well
+
+# in this case list is not empty
+print(bool([None]))  # list -> bool, True
+
+print(bool(None))  # None -> bool, False
+
+print('-' * 30)
+
+"""
+Scopes
+
+https://drive.google.com/file/d/1dy5IVxzkUautwfxYSwwy3R7LQNWmNePJ/view?usp=sharing
+https://realpython.com/python-scope-legb-rule
+
+Name lookup scopes:
+- Built-in scope: https://docs.python.org/3/library/functions.html
+    - Global (or module) scope
+        - Enclosing (or nonlocal) scope
+            - Local (or function) scope
+"""
+
+my_number = 1  # defined in a global scope
+
+
+def outer():
+    # enclosed / local scope
+    print('Outer function')
+    # when we remove the following line python will take my_number from the global scope
+    my_number = 100  # this line creates a new variable in the local scope and variable from the global scope is not changed
+    print(my_number)
+
+
+print(my_number)  # 1
+outer()
+print(my_number)
+
+print('-' * 30)
+
+"""
+We can create functions that can take any number of arguments.
+*args - it's a tuple of all positional arguments
+**kwargs - it's a dictionary where key is an argument name and value this argument value
+https://realpython.com/python-kwargs-and-args/
+"""
+print('Piotr', 'test', 1, 3.14, True)
+
+
+def counter(*args, **kwargs):
+    print('args: ', args)
+    print('kwargs: ', kwargs)
+
+
+counter(1, 2, 3, 'Tom', 3.14)
+counter(1, 2, 3, first_name='Piotr', last_name='GG')
+counter(title='Book', id=123)
