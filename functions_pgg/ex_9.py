98afa4c8fc577ddef25bb39a4bb57708931321d8
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-27 12:14:11 +0200

    ex 9.

---
+++ functions_pgg/ex_9.py
@@ -0,0 +1,13 @@
+"""
+Create a function that will return the n-th value of the Fibonacci sequence.
+https://en.wikipedia.org/wiki/Fibonacci_sequence
+
+Fibonacci sequence
+fib(0) = 0
+fib(1) = 1
+fib(n) = fib(n-1) + fib(n-2)
+"""
+
+def fib(n: int) -> int:
+    ...
+
