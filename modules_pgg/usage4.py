01ce6e11b2a0d5ce3ff9781036b6893253f80f0b
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-27 16:00:18 +0200

    modules

---
+++ modules_pgg/usage4.py
@@ -0,0 +1,8 @@
+"""
+modules_pgg/usage4.py
+"""
+from modules_pgg.my_module import my_string as my_string_tmp
+
+my_string = 'Hello Guys!'
+print(my_string)
+print(my_string_tmp)
---
+++ modules_pgg/usage5.py
@@ -0,0 +1,12 @@
+"""
+modules_pgg/usage5.py
+It's not a best practise as well. We should avoid
+importing everything...
+The good approach is to import only those elements I need.
+"""
+
+from modules_pgg.my_module import *
+
+print(my_string)
+print(my_numbers)
+print(square(3))
