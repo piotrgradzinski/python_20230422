8ebabc52a44eceff11e04bf13bf0a732d2b843a1
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-27 11:31:34 +0200

    ex 8.

--- functions_pgg/ex_8.py
+++ functions_pgg/ex_8.py
@@ -19,6 +19,21 @@ Check several values based on the wikipedia table.
 
 
 def factorial(n: int) -> int:
-    ...
+    result = 1
+
+    for i in range(1, n + 1):
+        # result *= i
+        result = result * i
+
+    return result
+
+
+def test_5():
+    assert factorial(5) == 120
+
+def test_0():
+    assert factorial(0) == 1
+
+
 
 
