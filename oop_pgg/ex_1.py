47d502c56eb0eb593da11f18d3461c1a495ea730
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-28 12:33:04 +0200

    files, ex 3.

---
+++ oop_pgg/ex_1.py
@@ -0,0 +1,16 @@
+"""
+Implement a Product class that stores information about the price, name and ID of the product.
+Implement a method that prints the product information to the console and returns a description as a string.
+
+Usage example:
+> product = Product(1, 'Water', 10.99)
+> product.print_info()
+Product "Water", id: 1, price: PLN 10.99
+
+Scenario:
+1. we create a class
+2. we add __init__ (constructor)
+3. we add attributes
+4. we add print_info(), get_info() methods
+5. extra: add type hinting and method documentation
+"""
---
+++ oop_pgg/oop_intro.py
@@ -0,0 +1,31 @@
+"""
+
+"""
+
+
+class Person:
+    def __init__(self, first_name=None, last_name=None):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def say_hello(self):
+        print(f'Hello! My name is {self.first_name} {self.last_name}!')
+
+
+person_piotr = Person('Piotr', 'GG')
+print(person_piotr)
+print(person_piotr.first_name)
+print(person_piotr.last_name)
+person_piotr.say_hello()
+
+person_john = Person()
+person_john.first_name = 'John'
+print(person_john)
+print(person_john.first_name)
+person_john.say_hello()
+
+person_mary = Person()
+person_mary.last_name = 'Doe'
+print(person_mary.first_name)
+print(person_mary.last_name)
+person_mary.say_hello()
