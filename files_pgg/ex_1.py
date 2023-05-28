b29ed52b2ea98e9a53c51304d2d451b129fd5290
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-28 09:36:00 +0200

    files, ex 1.

---
+++ files_pgg/ex_1.py
@@ -0,0 +1,15 @@
+"""
+Write a programme which prints out the contents of the indicated file
+with line numbers to the console.
+You can leverage test.txt file.
+
+Sample output:
+1: To be or not to be
+2: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+3: Duis tellus odio, hendrerit ut est id, convallis scelerisque eros.
+4: Mauris quis sem volutpat, scelerisque dolor et, efficitur ex.
+5: Nulla id tortor id mi faucibus dignissim. Curabitur bibendum sem erat.
+6: Maecenas faucibus tortor ac pulvinar sollicitudin. Donec nec semper elit.
+7: Cras in urna tellus. Donec velit enim, finibus in mauris ornare, luctus tempor nunc.
+8: Sed sed arcu in nisl tempor porta.
+"""
--- files_pgg/files_intro.py
+++ files_pgg/files_intro.py
@@ -18,7 +18,9 @@ https://docs.python.org/3/library/functions.html#open
 # file_handle = open('../test2.txt')
 
 # absolute path
-file_handle = open('/Users/gradzinski/dev_home/alx_python/szkolenia/2023_04_ZDL_Z/python_20230422/test2.txt')
+# file_handle = open('/Users/gradzinski/dev_home/alx_python/szkolenia/2023_04_ZDL_Z/python_20230422/test2.txt')
+
+file_handle = open('test.txt')
 
 # 2 step
 file_content = file_handle.read()
@@ -30,6 +32,53 @@ file_handle.close()
 print('-' * 30)
 
 # modes are really important and based on the mode you have your file opened in you can or can't do some operations
-file_handle = open('test.txt', 'w')
-file_handle.write('To be or not to be')
-file_handle.close()
+# file_handle = open('test.txt', 'w')
+# file_handle.write('To be or not to be')
+# file_handle.close()
+
+print('-' * 30)
+
+"""
+To make Python close the file for us automatically, we can use compound statement.
+"""
+
+with open('test.txt', 'r', encoding='utf-8') as file_handle:
+# with open('../collections_pgg/collections_intro.py', 'r', encoding='utf-8') as file_handle:
+    file_content = file_handle.read()
+    print(file_content)
+    print(file_handle.closed)  # False
+    print(file_handle.encoding)  # utf-8
+    print(file_handle.mode)  # r
+    print(file_handle.name)  # test.txt
+    print(file_handle.readable())  # True
+    print(file_handle.writable())  # False
+
+    file_handle.seek(0)  # moves the internal pointer to the beginning of the file
+    file_content = file_handle.read()
+    print(file_content)
+
+    file_handle.seek(0)
+    print(file_handle.read(15))
+
+    print('-' * 10)
+
+    file_handle.seek(0)
+    print(file_handle.readline().rstrip())  # by default when we have new line character at the end of the line in a file - it's also read.
+    print(file_handle.readline())
+    print(file_handle.readline())
+
+    print('-' * 10)
+
+    file_handle.seek(0)
+    lines = file_handle.readlines()  # list of lines from the file
+    print(lines)
+
+    print('-' * 10)
+
+    file_handle.seek(0)
+    for line in file_handle:
+        print(line)
+
+
+# once we leave the compound statement Python will close the file automatically.
+print(file_handle.closed)  # True
