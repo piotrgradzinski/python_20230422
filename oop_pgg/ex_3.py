b778c9bc225487d39f218541f9dd1dd6237a83aa
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-28 15:00:43 +0200

    oop, ex 3.

--- oop_pgg/ex_3.py
+++ oop_pgg/ex_3.py
@@ -5,8 +5,21 @@ and paying wages based on a preset hourly rate.
 UML class diagram: https://yuml.me/70e8bff8.png
 """
 
+
 class Employee:
-    pass
+    def __init__(self, first_name: str, last_name: str, rate: float):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.rate = rate
+        self.work_time = 0
+
+    def register_time(self, hours: int):
+        self.work_time += hours
+
+    def pay_salary(self) -> float:
+        salary = self.work_time * self.rate
+        self.work_time = 0
+        return salary
 
 
 employee = Employee(first_name='Jan', last_name='Nowak', rate=100.0)
