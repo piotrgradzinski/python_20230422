8fd4ab7880045a4e8aea98b8f1d4172efc0001c7
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-05-28 16:52:43 +0200

    oop, ex 4.

--- oop_pgg/ex_4_v2.py
+++ oop_pgg/ex_4_v2.py
@@ -26,12 +26,13 @@ Total: 60.00
 basket.count_products()  # 2
 basket.is_empty()  # False
 """
+from collections import defaultdict
 
 from ex_1 import Product
 
 class Basket:
     def __init__(self):
-        self.__items = dict()  # object of class Product as a key, quantity as a value
+        self.__items = defaultdict(int)
 
     def add_product(self, product: Product, quantity: int):
         if quantity <= 0:  # business decision whether quantity can be negative
@@ -40,16 +41,11 @@ class Basket:
         if not isinstance(product, Product):
             raise TypeError('You can only add Product to this basket.')
 
-        if product not in self.__items:
-            self.__items[product] = quantity
-        else:
-            self.__items[product] += quantity
+        self.__items[product] += quantity
 
     def count_total_price(self) -> float:
-        total_price = 0.0
-        for product, quantity in self.__items.items():
-            total_price += product.price * quantity
-        return total_price
+        return sum([product.price * quantity for product, quantity in self.__items.items()])
+
 
     def generate_receipt(self) -> str:
         receipt = ['Products']
@@ -61,18 +57,16 @@ class Basket:
     def count_products(self) -> int:
         return len(self.__items)
 
-    def is_empty(self) -> bool:  # is_empty is a method
-        if len(self.__items) > 0:
-            return False
-        else:
-            return True
+    @property
+    def is_empty(self) -> bool:
+        return not self.__items  # leveraging empty/non-empty dict to bool conversion
 
 
 water = Product(1, 'Water', 10.00)
 ice_cream = Product(2, 'Ice cream', 5.00)
 
 basket = Basket()
-print(basket.is_empty())  # True
+print(basket.is_empty)  # True
 basket.add_product(water, 5)
 basket.add_product(ice_cream, 2)
 
@@ -80,7 +74,7 @@ print(basket.count_total_price())  # 60.00
 receipt = basket.generate_receipt()
 print(receipt)
 print(basket.count_products())  # 2
-print(basket.is_empty())  # False
+print(basket.is_empty)  # False
 
 # _items in Basket class
 # print(basket._items.keys())  # Access to a protected member _items of a class
