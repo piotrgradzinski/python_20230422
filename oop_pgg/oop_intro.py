08209555cee5df9ac67e0fdc62a6530ca63a6563
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-06-17 10:57:07 +0200

    intro dunder methods.

--- oop_pgg/oop_intro.py
+++ oop_pgg/oop_intro.py
@@ -34,3 +34,44 @@ print(person_mary)
 print(person_mary.first_name)
 print(person_mary.last_name)
 person_mary.say_hello()
+
+print('-' * 30)
+
+"""
+Special methods, magic methods, dunder method (dunder - double underscore)
+https://piotr.gg/python/python3-dunder-methods-summary.html
+"""
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self) -> str:
+        return f'I am a Number and my value is {self.value}'
+
+    def __add__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value)
+        else:
+            return Number(self.value + other)
+
+
+n1 = Number(10)
+n2 = Number(20)
+
+# without __str__: <__main__.Number object at 0x100c0eed0>
+# with __str__: I am a number and my value is 10
+print(str(n1))
+print(str(n2))
+
+n3 = n1 + n2  # n1.__add__(n2)
+print(n3)
+
+n3 = n1.__add__(n2)
+print(n3)
+
+n4 = n1 + 10  # Number + int
+print(n4)
+
+n5 = n1 + 10.5  # Number + float
+print(n5)
