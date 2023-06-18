5b8b24a0bd1d18b726ab03fbdf30b8987b0d52b0
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-06-18 16:16:35 +0200

    stdlib, ex 7.

---
+++ stdlib_pgg/ex_7.py
@@ -0,0 +1,9 @@
+"""
+Using NBP endpoint http://api.nbp.pl/api/exchangerates/tables/A/?format=json
+fetch the information about current exchange rates
+and display the information to the user:
+currency code - rate
+
+for example
+EUR - 4.4583
+"""
