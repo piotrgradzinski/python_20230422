8c179f0e27e566534b0b212095dba9a3115a9da5
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-06-18 11:02:44 +0200

    re

--- stdlib_pgg/ex_2.py
+++ stdlib_pgg/ex_2.py
@@ -2,6 +2,12 @@
 Find all dates in dates.txt file that are in a format YEAR-MONTH-DAY,
 ex. 2023-06-18 and print the results.
 """
+import re
 
 with open('dates.txt', encoding='utf=8') as file_handle:
     content = file_handle.read()
+
+    results = re.findall(r"\d{4}-\d{2}-\d{2}", content)
+    print(results)
+    for result in results:
+        print(result)
