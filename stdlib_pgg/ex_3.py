7a6c05e82767709f0bf1b3e32c5f23cd57105877
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-06-18 12:00:14 +0200

    re, ex 3.

--- stdlib_pgg/ex_3.py
+++ stdlib_pgg/ex_3.py
@@ -1,4 +1,12 @@
 """
 Based on the urls.txt file extract unique domain names.
+
+regexp: ^https?://(www\.)?([^/]+)/.*
+
+- create an empty set to store the domains
+- read the file
+- for each line apply regular expression, retrieve the domain name from 2 group
+- add this match to domains set
+- display the results
 """
 
