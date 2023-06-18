1a4c4c91df251d99a49641eabb0e57341ae2b769
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-06-18 15:45:50 +0200

    stdlib, API.

---
+++ stdlib_pgg/ex_6.py
@@ -0,0 +1,13 @@
+"""
+VAT taxpayer list API
+
+Write a program that will check the VAT taxpayer list for information about a company
+based on its VAT number.
+
+Use https://wl-api.mf.gov.pl/api/search/nip/{VATID}?date=2023-06-18 endpoint where {VATID}
+has to be replaced with VAT ID of the company.
+
+For example:
+https://wl-api.mf.gov.pl/api/search/nip/5272642198?date=2023-06-18
+"""
+
