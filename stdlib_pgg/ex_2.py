f351c16513598c43c4b950bff24dcebb44045610
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-06-18 10:45:57 +0200

    re

--- stdlib_pgg/ex_2.py
+++ stdlib_pgg/ex_2.py
@@ -3,3 +3,5 @@ Find all dates in dates.txt file that are in a format YEAR-MONTH-DAY,
 ex. 2023-06-18 and print the results.
 """
 
+with open('dates.txt', encoding='utf=8') as file_handle:
+    content = file_handle.read()
