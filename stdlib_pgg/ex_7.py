619057febfe27670155ed4856f770b70d5263ec9
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-06-18 16:35:26 +0200

    stdlib, ex 7.

--- stdlib_pgg/ex_7.py
+++ stdlib_pgg/ex_7.py
@@ -7,3 +7,11 @@ currency code - rate
 for example
 EUR - 4.4583
 """
+import requests
+
+url = "http://api.nbp.pl/api/exchangerates/tables/A/?format=json"
+
+exchange_rates = requests.get(url).json()
+
+for rate in exchange_rates[0]['rates']:
+    print(rate['code'], '-', rate['mid'])
